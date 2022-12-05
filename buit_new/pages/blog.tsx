beab1fa1c0f918f7e45aea9c276b5489aee77275
Dmitr072 <shchderin01@mail.ru>
2022-12-05 17:56:03 +0500

    predMain

---
+++ buit_new/pages/blog.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Blog() {
+    return (
+        <>
+            <Link href="/">
+                Main
+            </Link>
+
+            <h1>Blog Page</h1>
+        </>
+
+    )
+}

106b36040dc7af7d8ed0ef8da8612f8af7a13308
Dmitr072 <96290549+Dmitr072@users.noreply.github.com>
2022-12-06 12:10:15 +0500

    Job

---
+++ buit_new/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+        <nav>
+            Navbar
+        </nav>
+
+    )
+}
--- buit_new/pages/blog.tsx
+++ buit_new/pages/blog.tsx
@@ -1,15 +1,17 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
 
 export default function Blog() {
     return (
-        <>
+        <div className={styles.container}>
             <Link href="/">
                 Main
             </Link>
 
             <h1>Blog Page</h1>
-        </>
+        </div>
 
     )
 }
